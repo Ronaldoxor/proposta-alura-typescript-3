fcf0ba0aad4457e33adc36317e5f33ddbd955ab9
Ronaldo <rronaldo.monteiro.almeida@gmail.com>
2025-12-20 16:53:58 -0300

    Adicionando comparabilidade entre listas

--- app/src/models/negociacoes.ts
+++ app/src/models/negociacoes.ts
@@ -1,7 +1,8 @@
 import { Imprimivel } from '../utils/imprimivel.js';
 import { Negociacao } from './negociacao.js';
+import { Comparavel } from '../interfaces/comparavel.js';
 
-export class Negociacoes implements Imprimivel{
+export class Negociacoes implements Imprimivel, Comparavel<Negociacoes>{
     private negociacoes: Negociacao[] = [];
 
     public adiciona(negociacao: Negociacao) {
@@ -15,4 +16,8 @@ export class Negociacoes implements Imprimivel{
     public paraTexto(): string{
         return JSON.stringify(this.negociacoes, null, 2);
     };
+
+    public ehIgual (negociacoes: Negociacoes): boolean {
+        return JSON.stringify(this.negociacoes) === JSON.stringify(negociacoes.lista());
+    }
 }
